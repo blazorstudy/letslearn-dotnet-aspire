abda95d40f298354d13eb3c8ff04cb2454ee5e02
김 진석(Kim, JinSeok) <iamjinseok@gmail.com>
2024-06-30 10:22:34 +0900

    한국 데모를 위한 폴더 생성
    - 2번째 데모에 대한 샘플 추가
    + 기상청과 연계 및 화면 동작

---
+++ demo-ko/demo-complete/demo-02/Api/Zone.cs
@@ -0,0 +1,5 @@
+﻿namespace Api;
+
+public record Zone(string Key, string Name, string State, string X, string Y);
+
+public record Forecast(string Name, string DetailedForecast);
---
+++ demo-ko/demo-start/demo-02/Api/Data/ForecastResponse.cs
@@ -0,0 +1,77 @@
+﻿namespace Api.Data;
+
+public class ApiResponse
+{
+	public Response response { get; set; }
+
+	public Forecast[] GetForecast(string baseDate)
+	{
+		var result = new List<Forecast>();
+
+		var fsctTimes = new List<string>();
+		for (int i = 0; i < 8; i++)
+		{
+			fsctTimes.Add($"{(i+10).ToString("00")}00");
+		}
+
+		var items = response.body.items.item;
+		foreach (var time in fsctTimes)
+		{
+			var tmp = items.FirstOrDefault(x => x.fcstDate == baseDate && x.fcstTime == time && x.category == "TMP")?.fcstValue;
+			var pop = items.FirstOrDefault(x => x.fcstDate == baseDate && x.fcstTime == time && x.category == "POP")?.fcstValue;
+			var sky = items.FirstOrDefault(x => x.fcstDate == baseDate && x.fcstTime == time && x.category == "SKY")?.fcstValue;
+			var skyValue = sky switch
+			{
+				"1" => "맑음",
+				"3" => "구름많음",
+				"4" => "흐림",
+				_ => "알수없음"
+			};
+
+			var title = $"{time.Substring(0, 2)}:{time.Substring(2, 2)}의 예보";
+			var detail = $"{skyValue}. 기온은 {tmp}로 예상. 비올 확률은 {pop}%";
+			result.Add(new Forecast(title, detail));
+
+		}
+		
+		return result.ToArray();
+	}
+}
+
+public class Response
+{
+	public Header header { get; set; }
+	public Body body { get; set; }
+}
+
+public class Header
+{
+	public string resultCode { get; set; }
+	public string resultMsg { get; set; }
+}
+
+public class Body
+{
+	public string dataType { get; set; }
+	public Items items { get; set; }
+	public int pageNo { get; set; }
+	public int numOfRows { get; set; }
+	public int totalCount { get; set; }
+}
+
+public class Items
+{
+	public Item[] item { get; set; }
+}
+
+public class Item
+{
+	public string baseDate { get; set; }
+	public string baseTime { get; set; }
+	public string category { get; set; }
+	public string fcstDate { get; set; }
+	public string fcstTime { get; set; }
+	public string fcstValue { get; set; }
+	public int nx { get; set; }
+	public int ny { get; set; }
+}
