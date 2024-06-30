aaa4b841c372c5c8c6a2fdb3e4ea4bcd21a38b86
김 진석(Kim, JinSeok) <iamjinseok@gmail.com>
2024-06-30 17:05:42 +0900

    - Node Hosting 패키지 추가 - AppHost에 React 프로젝트 추가

--- demo-ko/demo-complete/demo-06/AppHost/Program.cs
+++ demo-ko/demo-complete/demo-06/AppHost/Program.cs
@@ -1,7 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.Api>("api");
+
 builder.AddProject<Projects.MyWeatherHub>("myweatherhub")
 		.WithReference(apiService);
+
+builder.AddNpmApp( "react", "../MyReactApp")
+						.WithReference(apiService)
+						.WithEndpoint(targetPort: 3000, scheme: "http", env: "PORT");
 
 builder.Build().Run();
